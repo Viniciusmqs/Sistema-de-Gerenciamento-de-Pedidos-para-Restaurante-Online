14d9fcac53e01e76ea3d98d7798cd507ccd122f1
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2024-09-11 23:35:46 -0300

    Delete Pagamento.java

--- Pagamento.java
+++
@@ -1,7 +0,0 @@
-public class Pagamento {
-
-    public boolean processarPagamento(String metodoPagamento, double valor) {
-        System.out.println("Processando pagamento de R$" + valor + " via " + metodoPagamento);
-        return true;
-    }
-}
